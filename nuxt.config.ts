690652d249916923d7ccdaa8e6adb3a87c23035d
Ya-Alexey <y.alexey.a@mail.ru>
2024-02-15 19:30:49 +0300

    ended

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  runtimeConfig: {
+    baseSecret: '',
+    public: {
+      baseKey: '',
+    }
+  },
+  nitro: {
+    prerender: {
+      routes: ['/landing'],
+    },
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,22 @@
 <template>
-  <NuxtLink
-    class="underline font-medium"
-    :href="firstLesson.path"
-  >
-    Go to first lesson
-  </NuxtLink>
+  <div>
+    <NuxtLink
+      class="underline font-medium"
+      :href="firstLesson.path"
+    >
+      Go to first lesson
+    </NuxtLink>
+    <div>
+      <div>test</div>
+      <!-- <div>{{  }}</div> -->
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
   const firstLesson = await useFirstLesson();
+  const runtimeConfig = useRuntimeConfig()
+  console.log(runtimeConfig);
+  // console.log(runtimeConfig);
+  
 </script>
